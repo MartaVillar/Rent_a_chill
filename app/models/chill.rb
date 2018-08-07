aa3435193ea7d976d11b72b6129512879b1af2b2
Sabrina Catemario di Quadri <sabrinacdq1997@gmail.com>
2018-08-07 14:48:49 +0100

    worked on controller, view, routes, for new and create

--- app/models/chill.rb
+++ app/models/chill.rb
@@ -1,5 +1,5 @@
 class Chill < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
-  validates :title, :capacity, :price_per_hour, :location, presence: true
+  # validates :title, :capacity, :price_per_hour, :location, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  get 'chills', to: 'chills#index'
+  get 'chills/new', to: 'chills#new'
+  post 'chills', to: 'chills#create'
 end
