ba04c1aa78584d681d959369daddb3ee3e2dfd61
Marta Villar <martameneresvillar@hotmail.com>
2018-08-07 14:30:12 +0100

    seeds_booking wrong

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+require 'date'
 
 puts "Creating 20 users..."
+
+Booking.destroy_all
 
 20.times do
   User.new(
@@ -22,3 +24,17 @@ puts "Creating 20 users..."
 
   puts "Created#{User.count} users sucessfully!"
   puts "The last user created is #{User.last.name} born on #{User.last.birthday}"
+
+puts "Creating 20 bookins..."
+
+20.times do
+  Booking.new(
+    date: DateTime.new(2001,2,3,4,5,6),
+    chill_id: rand(1..20),
+    date: Faker::Time.between(50.days.ago, Date.today, :afternoon),
+    user_id: rand(1..20),
+  ).save!
+  end
+
+  puts "Created#{Booking.count} users sucessfully!"
+  puts "The last booking created is #{Booking.last}"
