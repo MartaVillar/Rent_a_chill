c8b8393d0a1eb96a38a91184d5bb85a7b1168b1a
TiagoFerrao <tiago.ferrao@gmail.com>
2018-08-08 11:49:04 +0100

    button color

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,8 @@ Rails.application.routes.draw do
 
   root to: 'chills#index'
 
-  get '/users/:id',     to: 'users#show'
-
  devise_for :users, :path_prefix => 'd'
  resources :users, :only =>[:show]
-
 
   get 'chills/new', to: 'chills#new'
   post 'chills', to: 'chills#create'
