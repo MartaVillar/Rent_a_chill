e13e772f668999368e38ed768cd3b51f3f49d517
Marta Villar <martameneresvillar@hotmail.com>
2018-08-09 15:17:33 +0100

    user connections done - user profile edit and show

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :bookings
-  has_many :chills, dependent: :destroy 
+  has_many :chills, dependent: :destroy
+  mount_uploader :photo, PhotoUploader
+
+  validates :name, presence: true
 end
