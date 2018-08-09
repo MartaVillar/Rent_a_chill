8697d86baa7c01d767b65045bfbadf27aa9ef650
Sabrina Catemario di Quadri <sabrinacdq1997@gmail.com>
2018-08-09 17:34:54 +0100

    search bar working can search by location or category

--- app/controllers/chills_controller.rb
+++ app/controllers/chills_controller.rb
@@ -1,8 +1,34 @@
 class ChillsController < ApplicationController
 skip_before_action :authenticate_user!, only: :index
 
+  # include PgSearch
+  # multisearchable against: [ :title, :syllabus ]
+
+  # pg_search_scope :search_by_title_and_syllabus,
+  #   against: [ :title,  ],
+  #   using: {
+  #     tsearch: { prefix: true } # <-- now `superman batm` will return something!
+  #   }
+
+
   def index
     @chills = Chill.all
+    if params[:query].present?
+      @chills_location = Chill.where("location ILIKE ?", "%#{params[:query]}%")
+      @chills_category = Chill.where("category ILIKE ?", "%#{params[:query]}%")
+      if @chills_category.length > @chills_location.length
+        @chills = @chills_category
+      elsif @chills_category.length + @chills_location.length == 0
+        @chills = []
+      else
+        @chills = @chills_location
+      end
+    end
+    if @chills.empty?
+      flash.now[:notice] = "Please try again..."
+      @chills = Chill.all
+    end
+    @chills
   end
 
   def new
