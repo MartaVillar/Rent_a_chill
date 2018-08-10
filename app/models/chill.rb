8158161daa07a63026979be5dddf8959bd90b9f3
Marta Villar <martameneresvillar@hotmail.com>
2018-08-10 13:02:49 +0100

    photos on chill and few more things

--- app/models/chill.rb
+++ app/models/chill.rb
@@ -2,6 +2,7 @@ class Chill < ApplicationRecord
   include PgSearch
    belongs_to :user
    has_many :bookings, dependent: :destroy
+   mount_uploader :photo, PhotoUploader
    validates :title, :capacity, :price_per_hour, :location, presence: true
    pg_search_scope :search_by_title_description,
        against: [ :title, :description ],
